55b86f6b3ac5623788c7dfd134cae29f21598a88
Ana Paula <apdsmenezes1809@gmail.com>
2022-08-26 19:17:21 -0300

    consegui

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -3,3 +3,9 @@ let email = "apdsmenezes1809@gmail.com"
 let profissao = "Estudante"
 let idade = 26
 let viagemDosSonhos = "Londres"
+ 
+console.log (nome)
+console.log (email)
+console.log (profissao)
+console.log (idade)
+console.log (viagemDosSonhos)
