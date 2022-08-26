be197df541ea1b27a9e4775b145421e50915a5df
Ana Paula <apdsmenezes1809@gmail.com>
2022-08-26 19:30:07 -0300

    programadora

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -4,8 +4,10 @@ let profissao = "Estudante"
 let idade = 26
 let viagemDosSonhos = "Londres"
  
+let mensagem = "Olá, " + nome + "! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para " + viagemDosSonhos + ". Confirma pra gente alguns dados? A sua idade é " + idade + ", você é " + profissao + "e o seu email " + email + "? Ficamos no aguardo. Muito obrigado e boa viagem! :D"
 console.log (nome)
 console.log (email)
 console.log (profissao)
 console.log (idade)
 console.log (viagemDosSonhos)
+console.log (mensagem)
